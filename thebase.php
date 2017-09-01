fb233ff5fe1a2d97a56580ef9dc57b904d5d69cd
Ryan Brinker <ryan.brinker@wisc.edu>
2017-09-01 06:20:08 -0500

    asdf

--- thebase.php
+++ thebase.php
@@ -3,8 +3,8 @@ $to      = 'rbrinker77@yahoo.com';
 $subject = 'Cam Alert';
 $message = 'Check Cam Video';
 $headers = 'From: rbrinker77@gmail.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
+    'Reply-To: rbrinker77@gmail.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers);
-?> 
+?>
