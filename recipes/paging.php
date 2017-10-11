9615151bd633028429dd56b67e3ee0e744de88ca
Ryan Brinker <ryan.brinker@wisc.edu>
2017-10-11 16:06:45 -0500

    missed class

--- recipes/paging.php
+++ recipes/paging.php
@@ -38,7 +38,7 @@ for($i = $sp; $i <= ($sp + $max -1);$i++) {
     continue;
   }
   if($paging_info['curr_page'] == $i) {
-    echo "<span class='bold'>".$i."</span>";
+    echo "<span class='pnum'>".$i."</span>";
   } else {
     echo "<a class='pnum' href='./results.php?p=".$i."' title='Page ".$i."'>".$i."</a>";
   }
