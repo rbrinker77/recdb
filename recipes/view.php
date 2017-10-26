122e3a67a8c51137fc2bf79a29c3033cbb98f15d
rbrinker77@yahoo.com <rbrinker77@yahoo.com>
2017-10-26 12:22:57 -0500

    changing view again

--- recipes/view.php
+++ recipes/view.php
@@ -79,7 +79,7 @@ foreach($dbConnection->query($recQuery) as $row)
 
 		if ($row[$instructNum] <> "")
 		{
-			echo "<div class=\"instructLine\"><label>".$count.".</label><span>".$row[$instructNum]."</span></div>";
+			echo "<div class=\"instructLine\"><div>".$count.".</div><div>".$row[$instructNum]."</div></div>";
 
 			$count++;
 		}
