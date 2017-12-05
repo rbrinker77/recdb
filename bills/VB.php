61b443e373f1ea1ad9ee04f2fbe2fb9dd79d3dd4
rbrinker77@yahoo.com <rbrinker77@yahoo.com>
2017-12-05 15:52:13 -0600

    Testing feeds

---
+++ bills/VB.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Display Twitter Feed On Website - LEVEL 2 - Live Demo</title>
+
+        <!-- Bootstrap CSS -->
+    <link href="libs/js/bootstrap/dist/css/bootstrap.css" rel="stylesheet" media="screen">
+</head>
+<body>
+
+  <a class="twitter-timeline"
+    href="https://twitter.com/RileyBell22"
+    data-width="300"
+    data-height="300">
+  </a>
+
+<!-- jQuery library -->
+<script src="libs/js/jquery.js"></script>
+
+<!-- bootstrap JavaScript -->
+<script src="libs/js/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="libs/js/bootstrap/docs-assets/js/holder.js"></script>
+
+<!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+</body>
+</html>
