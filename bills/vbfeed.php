29cdd65ea1903904cd89d1cf643240be2ff0082d
rbrinker77@yahoo.com <rbrinker77@yahoo.com>
2017-12-06 10:34:05 -0600

    logic

--- bills/vbfeed.php
+++ bills/vbfeed.php
@@ -14,25 +14,28 @@
 </head>
 <body>
 <?php
+  if ( !is_null($instaname) ) {
+    $instaResult = file_get_contents("https://www.instagram.com/".$instaname."/?__a=1");
+    $instas = json_decode($instaResult,true);
+    //var_dump($insta);
+    echo "<p class='instapic'>
+      <figure>
+        <b><figcaption>".$name."</figcaption></b>
+        <img src='".$instas['user']['profile_pic_url_hd']."' alt='".$instaname." Profile Pic' />
+      </figure>
+      </p>
+      <br />
+      <h2>Instagram</h2>";
 
-  $instaResult = file_get_contents("https://www.instagram.com/".$instaname."/?__a=1");
-  $instas = json_decode($instaResult,true);
-  //var_dump($insta);
-  echo "<p class='instapic'>
-    <figure>
-      <b><figcaption>".$name."</figcaption></b>
-      <img src='".$instas['user']['profile_pic_url_hd']."' alt='".$instaname." Profile Pic' />
-    </figure>
-    </p>
-    <br />
-    <h2>Instagram</h2>";
-
-  foreach ($instas['user']['media']['nodes'] as $insta) {
-    $postdate = date("m-d-Y @ H:i", $insta['date']);
-    echo "<br /><a class='instapost' href='".$insta['thumbnail_src']."'><img src='".$insta['thumbnail_resources'][0]['src']."' alt='".$postdate." - ".$insta['caption']."' /></a>";
+    foreach ($instas['user']['media']['nodes'] as $insta) {
+      $postdate = date("m-d-Y @ H:i", $insta['date']);
+      echo "<br /><a class='instapost' href='".$insta['thumbnail_src']."'><img src='".$insta['thumbnail_resources'][0]['src']."' alt='".$postdate." - ".$insta['caption']."' /></a>";
+    }
   }
 
-  echo "<h2>Twitter</h2></br /><a class='twitter-timeline' href='https://twitter.com/".$twittername."?ref_src=twsrc%5Etfw'>Tweets by ".$twittername."</a> <script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script>";
+  if ( !is_null($twittername) ) {
+    echo "<h2>Twitter</h2></br /><a class='twitter-timeline' href='https://twitter.com/".$twittername."?ref_src=twsrc%5Etfw'>Tweets by ".$twittername."</a> <script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script>";
+  }
 ?>
 
 </body>
