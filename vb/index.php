1fd444a74caddadee65c19679cdd8e249cc62b69
rbrinker77 <rbrinker77@yahoo.com>
2017-12-06 17:47:42 -0600

    test

--- vb/index.php
+++ vb/index.php
@@ -1,38 +1,41 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   include("./vbfeed.php");
 }
 else {
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Volleyball Feeds</title>
-  <link rel="stylesheet" href="../CSS/vb.css" type="text/css">
-<body>
-  <?php
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <title>Volleyball Feeds</title>
+    <link rel="stylesheet" href="../CSS/vb.css" type="text/css">
+  <body>
+    <?php
 
-include("../DB/dbconnect.php");
+  include("../DB/dbconnect.php");
 
-$vbroster = "SELECT * FROM vb ORDER BY name ASC;";
+  $vbroster = "SELECT * FROM vb ORDER BY name ASC;";
 
-foreach($dbConnection->query($vbroster) as $row)
-{
-	echo "<div class='thumbs'><form action='./vbfeed.php' method='post'>
-    <input type='image' src='./Images/".$row['name'].".jpg' title='".$row['name']."' alt='".$row['name']." image'>
-    <input type='hidden' name='name' value='".$row['name']."'>
-    <input type='hidden' name='jersey' value='".$row['jersey']."'>
-    <input type='hidden' name='twitter' value='".$row['twitter']."'>
-    <input type='hidden' name='instagram' value='".$row['instagram']."'>
-    </form>
-    </div>";
-}
+  foreach($dbConnection->query($vbroster) as $row)
+  {
+  	echo "<div class='thumbs'><form action='./vbfeed.php' method='post'>
+      <input type='image' src='./Images/".$row['name'].".jpg' title='".$row['name']."' alt='".$row['name']." image'>
+      <input type='hidden' name='name' value='".$row['name']."'>
+      <input type='hidden' name='jersey' value='".$row['jersey']."'>
+      <input type='hidden' name='twitter' value='".$row['twitter']."'>
+      <input type='hidden' name='instagram' value='".$row['instagram']."'>
+      </form>
+      </div>";
+  }
 
-$dbConnection = null;
+  $dbConnection = null;
 
-?>
-</body>
-</html>
+  ?>
+  </body>
+  </html>
 <?
 }
 ?>
