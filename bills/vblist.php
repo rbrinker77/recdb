c1bbbd53099b92b0c5f644bdd84d9705e4cc8ea5
rbrinker77@yahoo.com <rbrinker77@yahoo.com>
2017-12-06 10:25:08 -0600

    formatting

--- bills/vblist.php
+++ bills/vblist.php
@@ -4,14 +4,17 @@ include("./DB/dbconnect.php");
 
 $vbroster = "SELECT * FROM vb ORDER BY jersey ASC;";
 
-echo "<div class=\"roster\">";
+echo "<div class=\"roster\">
+  <h2>Volleyball Feeds</h2>";
 
 foreach($dbConnection->query($vbroster) as $row)
 {
 	echo "<form action='./vbfeed.php' type='post'>
-    <p><a href=''>".$row['name']."</a></p>
+    <a href=''>".$row['name']."</a>
     </form>";
 }
+
+echo "</div>";
 
 $dbConnection = null;
 
