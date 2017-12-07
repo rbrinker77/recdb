fbd66acee273ef31a5e86cb5cdbcfc79709cdf83
rbrinker77@yahoo.com <rbrinker77@yahoo.com>
2017-12-07 12:11:30 -0600

    divs

--- vb/vbfeed.php
+++ vb/vbfeed.php
@@ -32,27 +32,27 @@
     echo "<div class='instapic'>";
     echo "<img src='".$instas['user']['profile_pic_url_hd']."' title='".$instas['user']['biography']."' alt='".$instaname." Profile Pic' /></a>
       </div>
-      <div style='float:left;'>";
+      <div class='instapic'>";
 
-    if ( $instas['user']['external_url'] <> "" || $instas['user']['connected_fb_page'] <> "" ) {
+    if ( $instas['user']['external_url'] <> "" ) {
         echo "External: <a target='_blank' href='".$instas['user']['external_url']."'>".$instas['user']['external_url']."</a>";
-        echo "Facebook: <a target='_blank' href='".$instas['user']['connected_fb_page']."'>".$instas['user']['connected_fb_page']."</a>";
     }
 
-    echo "</div><br />";
+    echo "</div><br /><div>";
 
     if ( count($instas['user']['media']['nodes']) > 0 ) {
-      echo "<h2><a href='https://www.instagram.com/".$instaname."'>Instagram</a></h2>";
+      echo "<div><h2><a href='https://www.instagram.com/".$instaname."'>Instagram</a></h2>";
 
       foreach ($instas['user']['media']['nodes'] as $insta) {
         $postdate = date("m-d-Y @ H:i", $insta['date']);
         echo "<a class='instapost' target='_blank' href='".$insta['thumbnail_src']."'><img src='".$insta['thumbnail_resources'][0]['src']."' title='".$insta['caption']."' alt='".$postdate." - ".$insta['caption']."' /></a>";
       }
+
+      echo "</div>";
     }
   }
-
   if ( $twittername <> "" ) {
-    echo "</br /></br /></br /><a class='twitter-timeline' data-theme='dark' target='_blank' href='https://twitter.com/".$twittername."?ref_src=twsrc%5Etfw'>".$twittername."</a> <script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script>";
+    echo "</br /></br /></br /><div><a class='twitter-timeline' data-theme='dark' target='_blank' href='https://twitter.com/".$twittername."?ref_src=twsrc%5Etfw'>".$twittername."</a> <script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script></div>";
   }
 ?>
 
