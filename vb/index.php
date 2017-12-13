bc1f607863445d862086846d8498e566f9335e52
rbrinker77@yahoo.com <rbrinker77@yahoo.com>
2017-12-13 13:34:19 -0600

    RSS test

--- vb/index.php
+++ vb/index.php
@@ -32,7 +32,30 @@ else {
   }
 
   $dbConnection = null;
+//
+  $rss = new DOMDocument();
+  $rss->load('http://host.madison.com/search/?f=rss&t=article&c=sports/college&l=25&s=start_time&sd=desc'); // Set the blog RSS feed url here
 
+  $feed = array();
+  foreach ($rss->getElementsByTagName('item') as $node) {
+  	$item = array (
+  		'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+  		'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+  		'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+  		);
+  	array_push($feed, $item);
+  }
+
+
+  $limit = 10; // Set the number of articles to load here
+  for($n=0;$x<$limit;$n++) {
+  	$title = str_replace(' & ', ' &amp; ', $feed[$n]['title']);
+  	$link = $feed[$n]['link'];
+  	$description = $feed[$n]['desc'];
+  	echo '<p><b><a href="'.$link.'" title="'.$title.'">'.$title.'</a></b><br />';
+  	echo '<p>'.$description.'</p>';
+  }
+//
   ?>
   </body>
   </html>
