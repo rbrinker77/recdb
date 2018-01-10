b3d71e81656df833decaa1b8bd50b96ddbee9e6b
rbrinker77@yahoo.com <rbrinker77@yahoo.com>
2018-01-10 08:47:23 -0600

    Grew sheet size

--- budget/index.php
+++ budget/index.php
@@ -1,1 +1,1 @@
-<iframe width=100% height=100% src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQMGIwrzdyDW78BoU25PmGt_z9TKFIV-eUCG06zC9gJVO4ebEZ0gnr5ukFx-ZBjFnyVFS_7iYOUN6HO/pubhtml?gid=0&amp;single=true&amp;widget=false&amp;headers=false&amp;chrome=false&amp;range=B1:L33"></iframe>
+<iframe width=100% height=100% src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQMGIwrzdyDW78BoU25PmGt_z9TKFIV-eUCG06zC9gJVO4ebEZ0gnr5ukFx-ZBjFnyVFS_7iYOUN6HO/pubhtml?gid=0&amp;single=true&amp;widget=false&amp;headers=false&amp;chrome=false&amp;range=B1:L34"></iframe>
