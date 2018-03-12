982de8b4740a3864c2e628cffcbfb4789297c2f7
rbrinker77@yahoo.com <rbrinker77@yahoo.com>
2018-03-12 09:16:45 -0500

    errrr

--- recipes/view.php
+++ recipes/view.php
@@ -37,7 +37,7 @@ function randomRec($meat) {
 
 if (@$_GET['rdm'] == 'y') {
 	$recNum = randomRec($_GET['meat']);
-	echo "<input class=\"smallBlue\" name=\"cpLink\" type=\"button\" onclick=\"copyLink('https://recsite.ooguy.com/recipes/view.php?recNum=".$recNum."')\" value=\"Recipe link\" \">"
+	echo "<input class=\"smallBlue\" name=\"cpLink\" type=\"button\" onclick=\"copyLink('https://recsite.ooguy.com/recipes/view.php?recNum=".$recNum."')\" value=\"Recipe link\" \">";
 } else {
 	$recNum = $_GET['recNum'];
 	}
