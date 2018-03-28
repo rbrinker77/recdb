db0681c7958f4e1072343cc93d4a18f2b26b7f80
rbrinker77 <rbrinker77@yahoo.com>
2018-03-27 19:19:09 -0500

    div move

--- recipes/view.php
+++ recipes/view.php
@@ -55,12 +55,11 @@ foreach($dbConnection->query($recQuery) as $row)
 			<title>".html_entity_decode($row['recName'], ENT_QUOTES)."</title>
 		</head>
 		<body>
+			<div class=\"pageDiv\">
 		  <div>
 				<input class='smallBlue' name='cpLink' type='button' onclick=\"copyLink('twolinks')\" value='Copy Links' />
 				<textarea rows='2' type='text' class='cplink' readonly='readonly' id='twolinks'>http://recsite.ooguy.com/recipes/view.php?recNum=".$recNum."&#13;http://192.168.87.106/recipes/view.php?recNum=".$recNum."</textarea>
-				<p></p>
 			</div>
-			<div class=\"pageDiv\">
 			<div class=\"headerDiv big\">"
 				.html_entity_decode($row['recName'], ENT_QUOTES)."
 			</div>
