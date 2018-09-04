c5b1998dc8038ac33af68864457ce3f208d238e3
rbrinker77 <rbrinker77@yahoo.com>
2018-09-04 15:17:40 -0500

    trying

--- vb/vbfeed.php
+++ vb/vbfeed.php
@@ -2,6 +2,15 @@
 //error_reporting(E_ALL);
 //ini_set('display_errors', 1);
 
+function get_string_between($string, $start, $end){
+  $string = ' ' . $string;
+  $ini = strpos($string, $start);
+  if ($ini == 0) return '';
+  $ini += strlen($start);
+  $len = strpos($string, $end, $ini) - $ini;
+  return substr($string, $ini, $len);
+}
+
   if ( $_POST['jersey'] > 0 ) {
     $name=$_POST['name'].", #".$_POST['jersey'];
   }
@@ -23,30 +32,33 @@
   echo "<div class='instapic'><h3>".$name."</h3>";
 
   if ( $instaname <> "" ) {
-    function fetchData($url){
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
+
+    $instaResult = file_get_contents("https://www.instagram.com/".$instaname."/");
+    $instaString = get_string_between($instaResult, '<script type="text/javascript">window._sharedData = ', ';</script>');
+echo $instaString;
+    $instas = json_decode($instaString,true);
+
+    //$instas = json_decode($instaResult);var_dump($instas);die();
+    echo "<div>";
+    echo "<img src='".$instas['graphql']['user']['profile_pic_url_hd']."' title='".$instas['graphql']['user']['biography']."' alt='".$instaname." Profile Pic' /></a>
+      </div>
+      <div>";
+
+    if ( $instas['graphql']['user']['external_url'] <> "" ) {
+        echo "External: <a target='_blank' href='".$instas['graphql']['user']['external_url']."'>".$instas['graphql']['user']['external_url']."</a>";
     }
 
-    $result = fetchData("https://api.instagram.com/v1/users/".$instaname."/media/recent/?count=14");
+    echo "</div></div><br />";
 
+    if ( count($instas['graphql']['user']['edge_owner_to_timeline_media']['edges']) > 0 ) {
+      echo "<div><h2><a href='https://www.instagram.com/".$instaname."'>Instagram</a></h2>";
 
-    $result = json_decode($result);
-    foreach ($result->data as $post) {
-       if(empty($post->caption->text)) {
-         // Do Nothing
-       }
-       else {
-          echo '<a class="instagram-unit" target="blank" href="'.$post->link.'">
-          <img src="'.$post->images->low_resolution->url.'" alt="'.$post->caption->text.'" width="100%" height="auto" />
-          <div class="instagram-desc">'.htmlentities($post->caption->text).' | '.htmlentities(date("F j, Y, g:i a", $post->caption->created_time)).'</div></a>';
-       }
+      foreach ($instas['graphql']['user']['edge_owner_to_timeline_media']['edges'] as $insta) {
+        $postdate = date("m-d-Y @ H:i", $insta['node']['taken_at_timestamp']);
+        echo "<a class='instapost' target='_blank' href='".$insta['node']['thumbnail_src']."'><img src='".$insta['node']['thumbnail_resources'][0]['src']."' title='".$insta['node']['edge_media_to_caption']['edges'][0]['node']['text']."' alt='".$postdate." - ".$insta['node']['edge_media_to_caption']['edges'][0]['node']['text']."' /></a>";
+      }
 
+      echo "</div>";
     }
   }
   if ( $twittername <> "" ) {
