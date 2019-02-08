83d3bffdc15f8e149ad9ba262b08f18275047f17
rbrinker77 <rbrinker77@yahoo.com>
2019-02-08 14:08:27 -0600

    borken

--- whatthe.php
+++ whatthe.php
@@ -1,4 +1,9 @@
-<div width=100%>
-  <iframe width="420" height="315" align="middle" src="https://www.youtube.com/embed/tgbNymZ7vqY">
-  </iframe> 
-</div>
+<?php
+  echo "<div width=100% align='center'>";
+  if (isset($_POST['wtf']) {
+    echo "<iframe width='420' height='315' align='middle' src='https://www.youtube.com/embed/66g1_oTEWhIqE'></iframe>";
+  }
+  else {
+    echo "<form><input type='submit' name='wtf'><img src='../photo.jpg' alt='WTF?' height='60' width='60'> </input></form>";
+  }
+    echo "</div>";
