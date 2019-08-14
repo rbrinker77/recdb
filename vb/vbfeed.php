e8ea58bd7057d8d900a1248faf21567a9e260292
rbrinker77 <rbrinker77@yahoo.com>
2019-08-13 21:56:17 -0500

    Mistaken

--- vb/vbfeed.php
+++ vb/vbfeed.php
@@ -16,7 +16,7 @@ function get_string_between($string, $start, $end){
     $name=$_POST['jersey']." - ".$_POST['name'].", ".$_POST['position'];
   }
   else {
-    $name=$_POST['name'].", ".$_POST['position'];
+    $name=$_POST['name'];
   }
   $twittername=$_POST['twitter'];
   $instaname=$_POST['instagram'];
