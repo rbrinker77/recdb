fad452fec8b2b2bb490a8003e1f4e17924878c51
rbrinker77 <rbrinker77@yahoo.com>
2019-11-06 11:52:28 -0600

    split searches

--- vb/index.php
+++ vb/index.php
@@ -19,11 +19,11 @@ else {
 
   include("../DB/dbconnect.php");
 
-    $vbroster = "SELECT * FROM vb WHERE active=1 ORDER BY name ASC;";
-    $vbroster = "SELECT * FROM vb WHERE active=1 ORDER BY CASE WHEN name LIKE 'Badger%' THEN -1 ELSE name END,name;";
+      $vbroster = "SELECT * FROM vb WHERE name NOT LIKE 'Badger%' AND active=1 ORDER BY name ASC;";
+      $vbother = "SELECT * FROM vb WHERE name LIKE 'Badger%' AND active=1 ORDER BY name ASC;";
 
   echo "<div class='thumbs'>";
-
+//Roster loop
   foreach($dbConnection->query($vbroster) as $row)
   {
     if ($row['jersey'] > 0)
@@ -34,6 +34,21 @@ else {
     {
       $titleline = $row['name'];
     }
+  	echo "<div class='vbdiv'>
+      <form action='./index.php' method='post'>
+      <input type='image' class='vbimage' src='../Images/".$row['name'].".jpg' title='".$titleline."' alt='".$row['name']." image'>
+      <input type='hidden' name='name' value='".$row['name']."'>
+      <input type='hidden' name='jersey' value='".$row['jersey']."'>
+      <input type='hidden' name='position' value='".$row['position']."'>
+      <input type='hidden' name='twitter' value='".$row['twitter']."'>
+      <input type='hidden' name='instagram' value='".$row['instagram']."'>
+      </form>
+    </div>";
+  }
+//Non-roster links
+  foreach($dbConnection->query($vbother) as $row)
+  {
+    $titleline = $row['name'];
   	echo "<div class='vbdiv'>
       <form action='./index.php' method='post'>
       <input type='image' class='vbimage' src='../Images/".$row['name'].".jpg' title='".$titleline."' alt='".$row['name']." image'>
