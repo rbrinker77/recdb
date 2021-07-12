ee4eed9c6dc56bd90178c3f63f3ec8ab0e2d5c3a
Ryan Vinova <rbrinker@wisc.edu>
2021-07-12 17:19:48 -0500

    Move js more

--- files/upload.php
+++ files/upload.php
@@ -3,13 +3,11 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $targetDir = "/home/pbox/recs/web/files/uploads/";
-
+echo "<script type='text/JavaScript'>alert('".var_dump($_FILES)."');";
 //PHP code to upload file to server directory
 if (!empty($_FILES)) {
 	$temporaryFile = $_FILES['file']['tmp_name']; 
     $targetFile = $targetDir . basename($_FILES["file"]["name"]);
-
-    echo "<script type='text/JavaScript'>alert(".var_dump($_FILES).");";
 
 	if(!move_uploaded_file($temporaryFile,$targetFile))  {
 		echo "Error occurred while uploading the file to server!";	
