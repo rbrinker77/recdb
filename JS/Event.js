c3fe256aaae939eaaf61dffbd8f1e0175146c0b2
Ryan Vinova <rbrinker@wisc.edu>
2021-07-12 16:26:25 -0500

    Rm comments

--- JS/Event.js
+++ JS/Event.js
@@ -1,6 +1,3 @@
-// This script created by John Resig (http://ejohn.org/projects/flexible-javascript-events/) and altered by Quirksmode (http://www.quirksmode.org/blog/archives/2005/10/_and_the_winner_1.html)
-// Packaged to Event.js by fry @ friedcellcollective.
-
 function addEvent(obj,type,fn) {
 	if (obj.addEventListener) obj.addEventListener(type,fn,false);
 	else if (obj.attachEvent)	{
