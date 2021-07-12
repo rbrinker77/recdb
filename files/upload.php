a1438f63803d40d06cc83b326e78e757167d5a26
Ryan Vinova <rbrinker@wisc.edu>
2021-07-12 17:15:38 -0500

    Try js

--- files/upload.php
+++ files/upload.php
@@ -1,5 +1,4 @@
 <?php
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -7,12 +6,12 @@ $targetDir = "/home/pbox/recs/web/files/uploads/";
 
 //PHP code to upload file to server directory
 if (!empty($_FILES)) {
-    var_dump($_FILES);
 	$temporaryFile = $_FILES['file']['tmp_name']; 
-    $targetFile = $targetDir . $_FILES["file"]["name"];
+    $targetFile = $targetDir . basename($_FILES["file"]["name"]);
 
 	if(!move_uploaded_file($temporaryFile,$targetFile))  {
-		echo "Error occurred while uploading the file to server!";		
+		echo "Error occurred while uploading the file to server!";	
+        echo "alert(".var_dump($_FILES).")";
 	}
 }
 ?>
