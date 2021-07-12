48860dde637bc5e4efeac2f674d78e86fe26ca7d
Ryan Vinova <rbrinker@wisc.edu>
2021-07-12 17:41:09 -0500

    Diff js

--- files/index.php
+++ files/index.php
@@ -1,6 +1,6 @@
 <?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
+#error_reporting(E_ALL);
+#ini_set('display_errors', 1);
 ?>
 <!doctype html>
 <html>
@@ -15,6 +15,5 @@
             </form>  
             </div> 
         </div>
-        <div name="errors"></div>
     </body>
 </html>
--- files/upload.php
+++ files/upload.php
@@ -1,17 +1,17 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-print_r($_FILES);
-$targetDir = "/home/pbox/recs/web/files/uploads/";
-echo "<script type='text/JavaScript'>document.getElementById('errors').innerHTML = 'Top Level';</script>"
+#error_reporting(E_ALL);
+#ini_set('display_errors', 1);
+
+$targetDir = "uploads/";
+
 //PHP code to upload file to server directory
 if (!empty($_FILES)) {
 	$temporaryFile = $_FILES['file']['tmp_name']; 
     $targetFile = $targetDir . basename($_FILES["file"]["name"]);
-    echo "<script type='text/JavaScript'>document.getElementById('errors').innerHTML = 'Up files';</script>"
-	if(!move_uploaded_file($temporaryFile,$targetFile))  {
+
+    if(!move_uploaded_file($temporaryFile,$targetFile))  {
 		echo "Error occurred while uploading the file to server!";
-        echo "<script type='text/JavaScript'>document.getElementById('errors').innerHTML = 'File errors';</script>"
 	}
 }
 ?>
+<script>window.alert("Something");</script>
