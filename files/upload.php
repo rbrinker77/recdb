833ec000987399b755441f7e499fb233c6dc351f
Ryan Vinova <rbrinker@wisc.edu>
2021-07-12 17:18:28 -0500

    Move js

--- files/upload.php
+++ files/upload.php
@@ -9,9 +9,10 @@ if (!empty($_FILES)) {
 	$temporaryFile = $_FILES['file']['tmp_name']; 
     $targetFile = $targetDir . basename($_FILES["file"]["name"]);
 
+    echo "<script type='text/JavaScript'>alert(".var_dump($_FILES).");";
+
 	if(!move_uploaded_file($temporaryFile,$targetFile))  {
 		echo "Error occurred while uploading the file to server!";	
-        echo "<script type='text/JavaScript'>alert(".var_dump($_FILES).");";
 	}
 }
 ?>
