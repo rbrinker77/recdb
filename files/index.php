77019b20fc8a8cb3fa1179edeb8dabe19c0583a6
Ryan Vinova <rbrinker@wisc.edu>
2021-07-12 19:49:00 -0500

    New div

--- files/index.php
+++ files/index.php
@@ -4,9 +4,13 @@
         <link href="../CSS/styles.css" rel="stylesheet" type="text/css">
 
     <body >
-        <div class="pageDiv">
-            <button type="button" class="indexButton"><a href="aiden.php">Upload for Aiden</a></button>
-            <button type="button" class="indexButton"><a href="other.php">Upload other files</a></button>
+        <div class="indexContainer">
+            <div class="pageDiv">
+                <button type="button" class="indexButton"><a href="aiden.php">Upload for Aiden</a></button>
+            </div>
+            <div class="pageDiv">
+                <button type="button" class="indexButton"><a href="other.php">Upload other files</a></button>
+            </div>
         </div>
     </body>
 </html>
