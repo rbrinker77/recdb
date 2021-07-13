610b9f81f15f1ddaf0f043ccd493fbfef4a1b8a4
Ryan Vinova <rbrinker@wisc.edu>
2021-07-12 20:18:30 -0500

    old loc

--- files/aiden_upload.php
+++ files/aiden_upload.php
@@ -2,8 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$rootDir = realpath($_SERVER["DOCUMENT_ROOT"]);
-$targetDir = $rootDir."/files/uploads/Aiden/";
+$targetDir = "./uploads/Aiden/";
 
 //PHP code to upload file to server directory
 if (!empty($_FILES)) {
