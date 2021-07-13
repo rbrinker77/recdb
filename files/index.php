192fa3097e2b3e8e6e77abfc86abd9ca977be38b
Ryan Vinova <rbrinker@wisc.edu>
2021-07-12 19:41:03 -0500

    New front

---
+++ files/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <link href="../CSS/styles.css" rel="stylesheet" type="text/css">
+
+    <body >
+        <div>
+            <a href="aiden.php">Upload for Aiden</a>
+            <a href="other.php">Upload other files</a> 
+        </div>
+    </body>
+</html>
