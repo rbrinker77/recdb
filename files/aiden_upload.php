6e59322e53b4dee6c8ef98a9a5bd17e0351f5567
Ryan Vinova <rbrinker@wisc.edu>
2021-07-13 16:22:59 -0500

    Folder work

--- files/aiden_upload.php
+++ files/aiden_upload.php
@@ -2,21 +2,26 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$targetDir = $_SERVER['DOCUMENT_ROOT']."/files/uploads/Aiden/";
+$targetDir = $_SERVER['DOCUMENT_ROOT']."/files/uploads/Aiden";
+$yearDir = $targetDir."/".date("Y");
+$monthDir = $targetDir."/".date("n");
 
 //PHP code to upload file to server directory
 if (!empty($_FILES)) {
 	$temporaryFile = $_FILES['file']['tmp_name']; 
-    $targetFile = $targetDir . basename($_FILES["file"]["name"]);
+
+	if (!file_exists($yearDir)) {
+		mkdir($yearDir);
+		mkdir($monthDir);
+	}
+	else if (!file_exists($monthDir)) {
+		mkdir($monthDir);
+	}
+
+    $targetFile = $monthDir . "/" . basename($_FILES["file"]["name"]);
 
     if(!move_uploaded_file($temporaryFile,$targetFile))  {
 		echo "Error occurred while uploading the file to server!";
 	}
 }
-$myfile = fopen($targetDir."newfile.txt", "w") or die("Unable to open file!");
-$txt = "John Doe\n";
-fwrite($myfile, $txt);
-$txt = "Jane Doe\n";
-fwrite($myfile, $txt);
-fclose($myfile);
 ?>
