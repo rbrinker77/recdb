f493544f5c26a71453b5001ee131078208bc7f13
Ryan Vinova <rbrinker@wisc.edu>
2021-07-12 19:50:45 -0500

    divs

--- files/index.php
+++ files/index.php
@@ -4,11 +4,11 @@
         <link href="../CSS/styles.css" rel="stylesheet" type="text/css">
 
     <body >
-        <div class="indexContainer">
-            <div class="pageDiv">
+        <div class="pageDiv">
+            <div class="indexContainer">
                 <button type="button" class="indexButton"><a href="aiden.php">Upload for Aiden</a></button>
             </div>
-            <div class="pageDiv">
+            <div class="indexContainer">
                 <button type="button" class="indexButton"><a href="other.php">Upload other files</a></button>
             </div>
         </div>
