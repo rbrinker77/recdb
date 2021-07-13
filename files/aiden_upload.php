e55dc75d7a998113a5e9767a282f9af5176077bc
Ryan Vinova <rbrinker@wisc.edu>
2021-07-12 20:59:11 -0500

    top loc

--- files/aiden_upload.php
+++ files/aiden_upload.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$targetDir = "uploads/Aiden/";
+$targetDir = "../files/uploads/Aiden/";
 
 //PHP code to upload file to server directory
 if (!empty($_FILES)) {
@@ -13,21 +13,10 @@ if (!empty($_FILES)) {
 		echo "Error occurred while uploading the file to server!";
 	}
 }
-$dir_path = "../files/uploads";
-
-if (is_dir($dir_path)) {
-    if ($dir_handler = opendir($dir_path)) {
-        while (($file = readdir($dir_handler)) !== false) {
-            echo "filename: $file : filetype: " . filetype($dir_path . $file) . "\n";
-        }
-        closedir($dir_handler);
-    }
-}
-
-#$myfile = fopen($targetDir."newfile.txt", "w") or die("Unable to open file!");
-#$txt = "John Doe\n";
-#fwrite($myfile, $txt);
-#$txt = "Jane Doe\n";
-#fwrite($myfile, $txt);
-#fclose($myfile);
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
 ?>
