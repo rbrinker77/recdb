08258e3a06584a0344872daf811053d4aa2f0acf
Ryan Vinova <rbrinker@wisc.edu>
2021-10-04 15:51:17 -0500

    Button work

--- files/uploads/index2.php
+++ files/uploads/index2.php
@@ -1,3 +1,9 @@
+<!doctype html>
+<html>
+    <head>
+        <link href="../CSS/styles.css" rel="stylesheet" type="text/css">
+        <link rel="icon" type="image/png" href="/favicon.ico">
+    </head>
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -6,3 +12,11 @@ $parentDir = dirname( dirname(__FILE__) );
 echo $parentDir;
 
 ?>
+    <body >
+        <div class="pageDiv">
+            <div class="indexContainer">
+                <button type="button" class="smallGreen"><a class="indexes" href="<?echo $parentDir;?>">Up a level</a></button>
+            </div>
+        </div>
+    </body>
+</html>
