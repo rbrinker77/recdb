dd85a6631252333cfa82ddfa0b965546ce225aea
Ryan Vinova <rbrinker@wisc.edu>
2021-10-05 09:57:30 -0500

    Change to copy

--- files/aiden_upload.php
+++ files/aiden_upload.php
@@ -7,7 +7,7 @@ $yearDir = $targetDir."/".date("Y");
 $monthDir = $yearDir."/".date("n");
 $buildyear = false;
 $buildmonth = false;
-$filetxt = '<?php include "/var/www/html/files/uploads/index.php"; ?>';
+$filename = '/var/www/html/files/uploads/index.php';
 
 //PHP code to upload file to server directory
 if (!empty($_FILES)) {
@@ -26,16 +26,16 @@ if (!empty($_FILES)) {
 
 	if($buildyear) {
 		$yearname = $yearDir."/index.php";
-		$yearfile = fopen($yearname, "w") or die("Unable to open year file!");
-		fwrite($yearfile, $filetxt);
-		fclose($yearfile);
+		if (!copy($filename, $yearname)) {
+			echo "Failed to copy $yearname...\n";
+		}
 	}
 
 	if($buildmonth) {
 		$monthname = $monthDir."/index.php";
-		$monthfile = fopen($monthname, "w") or die("Unable to open month file!");
-		fwrite($monthfile, $filetxt);
-		fclose($monthfile);
+		if (!copy($filename, $monthname)) {
+			echo "Failed to copy $monthname...\n";
+		}
 	}
 
     $targetFile = $monthDir . "/" . basename($_FILES["file"]["name"]);
