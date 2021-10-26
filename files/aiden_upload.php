14804f2c9d1b4457c967ad5610115d778ec239df
Ryan Vinova <rbrinker@wisc.edu>
2021-10-26 13:06:08 -0500

    Redo function call

--- files/aiden_upload.php
+++ files/aiden_upload.php
@@ -47,9 +47,7 @@ if (!empty($_FILES)) {
     if(!move_uploaded_file($temporaryFile,$targetFile))  {
 		echo "Error occurred while uploading the file to server!";
 	} else {
-		if(!make_thumb($targetFile,$thumbFile,$width))  {
-			echo "Error occurred while creating thumbnail!";
-		}
+		make_thumb($targetFile,$thumbFile,$width);
 	}
 }
 ?>
