3c9407efeb9b6d22c2ea47b3e95ad0f369811a32
Ryan Vinova <rbrinker77@yahoo.com>
2022-01-17 10:57:40 -0600

    Field format

--- reviews/index.php
+++ reviews/index.php
@@ -23,7 +23,7 @@ ini_set('display_errors', 1);
   	echo "<div>
       <form action='./index.php' method='post'>
         <h3>".$row['name']."</h3>
-        <input type='textbox' name='desc' />
+        <input type='text' name='desc' spellcheck='yes' size='50'/>
         <input type='hidden' name='id' value='".$row['id']."' />
         <input type='submit' name='submit' value='Send' />
       </form>
