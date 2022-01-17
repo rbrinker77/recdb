017441697d3efa3ac708c61a8d49e5a76b58f304
Ryan Vinova <rbrinker77@yahoo.com>
2022-01-17 14:47:26 -0600

    hide query

--- reviews/index.php
+++ reviews/index.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $addevent = "INSERT INTO events (type, description) VALUES ("..", "..");";
+  //$addevent = "INSERT INTO events (type, description) VALUES ("..", "..");";
   var_dump($_POST);
 }
 ?>
