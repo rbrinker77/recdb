e376502b323c6d955f837dd7cd7e48a961743349
Ryan Vinova <rbrinker77@yahoo.com>
2022-01-17 13:07:47 -0600

    New table

--- reviews/index.php
+++ reviews/index.php
@@ -18,23 +18,18 @@ ini_set('display_errors', 1);
   foreach($dbConnection->query($typelist) as $row)
   {
     $idnum=$row['id'];
-    $eventlist[$idnum] = "SELECT * FROM events WHERE id='".$idnum."' ORDER BY date DESC;";
+    $eventlist[$idnum] = "SELECT * FROM events WHERE type='".$idnum."' ORDER BY date DESC;";
+
+    echo "<table><tr><td>".$row['type']."</td></tr>";
+    echo "<table><tr><td>Date</td><td>Description</td></tr>";
+
+    foreach ($eventlist[$idnum] as $eventrow)
+    {
+      echo "<td>".$eventrow['date']."</td><td>".$eventrow['description']."</td>";
+    }
   }
 
   echo "<div>";
-
-  foreach($dbConnection->query($typelist) as $row)
-  {
-  	echo "<div>
-      <form action='./index.php' method='post'>
-        <h3>".$row['name']."</h3>
-        <input type='text' name='desc' spellcheck='true' size='50'/>
-        <input type='hidden' name='id' value='".$row['id']."' />
-        <input type='submit' name='submit' value='Send' />
-      </form>
-    </div>";
-  }
-
 
   foreach($dbConnection->query($typelist) as $row)
   {
