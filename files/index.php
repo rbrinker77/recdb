6207e26c1881df58345eb6d78dded4ae94db8d1e
Ryan Vinova <rbrinker@wisc.edu>
2022-03-28 11:28:04 -0500

    Rm nas upload

--- files/index.php
+++ files/index.php
@@ -12,9 +12,6 @@
             <div class="indexContainer">
                 <button type="button" class="indexButton"><a href="other.php">Other Photos/Vids</a></button>
             </div>
-            <div class="indexContainer">
-                <button type="button" class="indexButton"><a href="file.php">Files</a></button>
-            </div>
             <br /><br />
             <div class="indexContainer">
                 <button type="button" class="smallGreen"><a class="indexes" href="/files/uploads/">Photos</a></button>
