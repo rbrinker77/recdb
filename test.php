540efb1d7c795335f15003c0a71edffb37e8279d
Ryan Vinova <ryan.vinova@wisc.edu>
2022-09-22 08:51:16 -0500

    Mail test

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+
+$message = "Testing a thing";
+$subject = "test google play";
+$email   = "a.woodward.k@gmail.com";
+$email   = "rbrinker77@yahoo.com";
+
+mail($email, $subject, $message);
+
+?>
