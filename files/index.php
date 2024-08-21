c985982d9c46549000fe83be2a97a42283d4ca57
rbrinker77 <rbrinker77@yahoo.com>
2024-08-21 08:44:33 -0500

    Reform pages

--- files/index.php
+++ files/index.php
@@ -7,14 +7,13 @@
     <body >
         <div class="pageDiv">
             <div class="indexContainer">
-                <button type="button" class="indexButton"><a href="aiden.php">Aiden Photos/Vids</a></button>
+                <button type="button" class="indexButton"><a href="page.php?title=Family">Family files</a></button>
             </div>
             <div class="indexContainer">
-                <button type="button" class="indexButton"><a href="other.php">Other Photos/Vids</a></button>
+                <button type="button" class="indexButton"><a href="page.php?title=Aidens">Aiden's files</a></button>
             </div>
-            <br /><br />
             <div class="indexContainer">
-                <button type="button" class="smallGreen"><a class="indexes" href="/files/uploads/">Photos</a></button>
+                <button type="button" class="indexButton"><a href="page.php?title=Other">Other files</a></button>
             </div>
         </div>
     </body>
---
+++ files/uploads.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include_once($path);
+
+$title      = $_GET['title'];
+$targetDir  = "/media/uploads/".$title;
+$yearDir    = $targetDir."/".date("Y");
+$monthDir   = $yearDir."/".date("F");
+$buildyear  = false;
+$buildmonth = false;
+$width      = 200;
+
+//PHP code to upload file to server directory
+if (!empty($_FILES)) {
+	$temporaryFile = $_FILES['file']['tmp_name'];
+
+	if (!file_exists($yearDir)) {
+		mkdir($yearDir);
+		mkdir($monthDir);
+	}
+	else if (!file_exists($monthDir)) {
+		mkdir($monthDir);
+	}
+
+	$targetFile = $monthDir . "/" . basename($_FILES["file"]["name"]);
+	$mimeType = mime_content_type($_FILES["file"]["name"]);
+	$fileType = explode('/', $mimeType)[0]; // video|image
+
+	if(!move_uploaded_file($temporaryFile,$targetFile))  {
+		echo "Error occurred while uploading the file to server!";
+	}
+}
+?>
